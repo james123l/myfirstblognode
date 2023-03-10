d19dc6d5afb2a3e49561301177c5edfeace96d14
jamesjavanyc` <1931736857@qq.com>
2023-03-10 01:37:04 -0500

    engine

--- index.js
+++ index.js
@@ -35,11 +35,7 @@ const upload= multer({storage:storage});
 // 绑定路径和中间件
 //app.use(path,callback)中的callback既可以是router对象又可以是函数
 //app.get(path,callback)中的callback只能是函数
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
-app.use("/api/posts",postRoute);
-app.use("/api/catagories",catagoriesRoute);
-app.use("./images",express.static(path.join(__dirname,"/images")));
+app.use("/", require("./routes"))
 app.post("/api/upload",upload.single("file"),(req,res)=>{
     res.status(200).json("File has been uploaded");
 })
