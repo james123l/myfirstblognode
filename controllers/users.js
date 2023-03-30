e51f5e2e9e6e2813e6e18fbdfe95935cea888be7
jamesjavanyc` <1931736857@qq.com>
2023-03-29 23:11:43 -0400

    debug

--- controllers/users.js
+++ controllers/users.js
@@ -5,34 +5,30 @@ const bcrypt = require("bcrypt");
 const update = async (req, res) => {
     if (!req.params.token) {
         res.status(401).json("Not login.")
-        return 
+        return
     }
-    if(req.body.userId === req.params.token){
-        if(req.body.password){
-            const salt = await bcrypt.genSalt(10);
-            newpassword = await bcrypt.hash(req.body.password,salt);
-            try{
-                const original = await User.findById(req.body.userId);
-                original.password = newpassword;
-                await User.updateOne({_id:req.body.userId}, original)
-                res.status(200).json();
-            } catch (err) {
-                console.log(err)
-                res.status(500).json(err);
-            }
+    if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        newpassword = await bcrypt.hash(req.body.password, salt);
+        try {
+            const original = await User.findById(req.body.userId);
+            original.password = newpassword;
+            await User.updateOne({ _id: req.body.userId }, original)
+            res.status(200).json();
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err);
         }
-    }else{
-        res.status(401).json("You can only update your account.");
     }
 }
-const get = async (req,res)=>{
-    try{
+const get = async (req, res) => {
+    try {
         const user = await User.findById(req.params.token);
         const { password, ...others } = user._doc;
         res.status(200).json(others);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 }
 
-module.exports = {get,update}
+module.exports = { get, update }
